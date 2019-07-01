0b1a160520a73833a4e63ac590858400dbc22e8f
Shogo N <shogonir@gmail.com>
2019-07-01 23:26:11 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h1>Nuxt TypeScript Vuetify project</h1>
+  </section>
+</template>
+
+
+
+<script lang="ts">
+
+import { Component, Vue } from 'nuxt-property-decorator'
+import { State } from 'vuex-class'
+
+@Component({})
+export default class extends Vue {
+}
+
+</script>
+
+
+
+<style scoped>
+</style>
---
+++ store/index.ts
@@ -0,0 +1,11 @@
+import { RootState } from "~/types";
+import { MutationTree, ActionTree } from "vuex";
+
+export const state = (): RootState => ({
+})
+
+export const mutations: MutationTree<RootState> = {
+}
+
+export const actions: ActionTree<RootState, RootState> = {
+}
